e6225b2dc389b4126ea094b3224f43d20820c4c1
GitHub Action <action@github.com>
2025-03-10 17:32:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexivan04/assesmentplugin/MyBundle.kt
+++ src/main/kotlin/com/github/alexivan04/assesmentplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alexivan04.assesmentplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alexivan04/assesmentplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/alexivan04/assesmentplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alexivan04.assesmentplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
